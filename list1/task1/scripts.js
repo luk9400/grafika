0674e1fe1f7681e0c6e7fb56b445299f4b5b46f3
luk9400 <luk9400@gmail.com>
2019-10-28 21:18:16 +0100

    cos

--- list1/task1/scripts.js
+++ list1/task1/scripts.js
@@ -1,12 +1,16 @@
-let maxX = 500;
-let maxY = 400;
+const canvas = document.getElementById("myCanvas");
+const context = canvas.getContext("2d");
+const maxX = 500;
+const maxY = 400;
 let currentX = 100;
-let currentY = maxY - 100;
+let currentY = 100;
 let currentAngle = 0;
 
-let canvas = document.getElementById("myCanvas");
-let ctx = canvas.getContext("2d");
-ctx.moveTo(currentX, currentY);
+function translateCoordinates() {
+    context.translate(0, maxY);
+    context.scale(1, -1);
+    context.moveTo(currentX, currentY);
+}
 
 function degree() {
     return -(currentAngle / 360) * 2 * Math.PI;
@@ -15,8 +19,8 @@ function degree() {
 function fd(dist) {
     currentX = currentX + dist * Math.cos(degree());
     currentY = currentY + dist * Math.sin(degree());
-    ctx.lineTo(currentX, currentY);
-    ctx.stroke();
+    context.lineTo(currentX, currentY);
+    context.stroke();
 }
 
 function right(angle) {
@@ -26,6 +30,8 @@ function right(angle) {
 function left(angle) {
     currentAngle = (currentAngle + angle) % 360;
 }
+
+translateCoordinates();
 
 for (let i = 0; i < 6; i++) {
     fd(50);
